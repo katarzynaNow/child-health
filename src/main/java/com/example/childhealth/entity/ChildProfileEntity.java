7d430acfa750e40a9270889661fcfb52e191057b
Katarzyna <noworyta.katarzyna@wp.pl>
2023-12-11 21:23:20 +0100

    Child profile entity

---
+++ src/main/java/com/example/childhealth/entity/ChildProfileEntity.java
@@ -0,0 +1,60 @@
+package com.example.childhealth.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.type.SqlTypes;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name="children_profiles")
+@Getter
+@Setter
+public class ChildProfileEntity {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name="UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @JdbcTypeCode(SqlTypes.CHAR)
+    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false, name = "id")
+    private UUID id;
+
+    @NotNull
+    @NotBlank
+    @Size(max = 50)
+    @Column(length = 50, name = "name")
+    private String name;
+
+    @Column(name = "birth_date")
+    private LocalDateTime birthDate;
+
+    @Lob
+    @Column(name = "picture")
+    private byte[] picture;
+
+  /*  @OneToMany(mappedBy = "child", cascade = CascadeType.ALL)
+    private List<DiseaseEntity> disease;
+
+    @OneToMany(mappedBy = "child", cascade = CascadeType.ALL)
+    private List<AppointmentEntity> appointment;
+
+    @OneToMany(mappedBy = "child", cascade = CascadeType.ALL)
+    private List<Vaccination> vaccination;*/
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
