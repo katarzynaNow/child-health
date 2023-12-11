4f676859148555d1d27309b51c9cbccb1c306f58
Katarzyna <noworyta.katarzyna@wp.pl>
2023-12-11 21:08:22 +0100

    Initial commit

---
+++ src/main/java/com/example/childhealth/ChildHealthApplication.java
@@ -0,0 +1,13 @@
+package com.example.childhealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChildHealthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChildHealthApplication.class, args);
+    }
+
+}
