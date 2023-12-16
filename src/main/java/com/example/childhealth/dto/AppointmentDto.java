3045559110fd43bab7100cab3acadae82d61a8c1
Katarzyna <noworyta.katarzyna@wp.pl>
2023-12-16 21:03:07 +0100

    appointment dto

---
+++ src/main/java/com/example/childhealth/dto/AppointmentDto.java
@@ -0,0 +1,31 @@
+package com.example.childhealth.dto;
+
+import com.example.childhealth.entity.DiseaseEntity;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Builder
+@Data
+public class AppointmentDto {
+
+    private LocalDateTime date;
+
+    @Size(min=2, max=30)
+    private String diagnosis;
+
+    @Size(min=2, max=30)
+    private String medicines;
+
+    private boolean antibiotic;
+
+    @Size(max=200)
+    private String notes;
+
+    @NotNull
+    private DiseaseEntity disease;
+
+}
