f913161edc357fbb2aa680d3fc4ce2d991b97d0a
Katarzyna <noworyta.katarzyna@wp.pl>
2024-01-04 10:28:08 +0100

    vaccination controller

---
+++ src/main/java/com/example/childhealth/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.example.childhealth.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+
+    @GetMapping("test")
+    public String test(){
+        return "Test";
+    }
+
+    @GetMapping("/")
+    public String showIndex(){
+        return "redirect:profiles";
+    }
+}
---
+++ src/main/java/com/example/childhealth/controller/VaccinationController.java
@@ -0,0 +1,61 @@
+package com.example.childhealth.controller;
+
+import com.example.childhealth.entity.VacStatus;
+import com.example.childhealth.entity.Vaccination;
+import com.example.childhealth.service.ChildProfileService;
+import com.example.childhealth.service.VaccinationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@Controller
+@RequestMapping("/profiles/{profileId}/vaccination")
+@RequiredArgsConstructor
+public class VaccinationController {
+    @Value("${app.title.vaccination}")
+    private String title;
+
+    private final VaccinationService vaccinationService;
+    private final ChildProfileService childProfileService;
+
+    @GetMapping
+    public String vaccinationSchedulePage(Model model, @RequestParam(required = false) UUID editedId,
+                                          @PathVariable UUID profileId){
+        List<Vaccination> vaccinations = vaccinationService.findAll();
+
+        model.addAttribute("vaccinations", vaccinations);
+        model.addAttribute("title", title);
+        model.addAttribute("editedId", editedId);
+        model.addAttribute("profile", childProfileService.findById(profileId));
+
+
+        if(editedId != null) {
+            model.addAttribute("editVaccination", vaccinationService.findById(editedId));
+        }
+
+        return "vaccination";
+    }
+
+    @GetMapping("/update")
+    public String updateStatus(@RequestParam UUID id, @RequestParam VacStatus vacStatus){
+
+        Vaccination existing = vaccinationService.findById(id);
+        existing.setStatus(vacStatus);
+        vaccinationService.save(existing);
+        return "redirect:/profiles/{profileId}/vaccination";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String editNotes(String notes, @PathVariable UUID id) {
+        Vaccination existing = vaccinationService.findById(id);
+        existing.setNotes(notes);
+
+        vaccinationService.save(existing);
+        return "redirect:/profiles/{profileId}/vaccination";
+    }
+}
